b0b69e4985b66c50beff3cc719cf3a642de93aef
Quyen Nguyen <ntquyen98@gmail.com>
2020-05-12 23:31:24 +0700

    check security

--- utils/security.js
+++ utils/security.js
@@ -1,5 +1,35 @@
+import dataConfig from '../config';
+const moment = require('moment');
+const crypto = require('crypto');
+
 module.exports = {
-  checkPartner: () => {},
-  isPackageNew: () => {},
-  isOriginPackage: () => {},
+  checkPartner: (securityKey) => {
+    if (securityKey) {
+      let partnerArray = dataConfig ? dataConfig.partner : [];
+      if (Array.isArray(partnerArray) && partnerArray.length > 0) {
+        let partner = partnerArray.filter((item) => {
+          return item.securityKey == securityKey;
+        })
+        if (Array.isArray(partner) && partner.length > 0) return partner[0];
+      }
+    }
+    return false;
+  },
+
+  isPackageNew: (timestamp) => {
+    return moment().unix() - timestamp <= 60
+  },
+
+  isOriginPackage: (data, timestamp, sig, securityKey) => {
+    let _data = JSON.stringify(data, null, 2);
+    let endCodeSecurity = md5(timestamp + _data + securityKey);
+    return sig == endCodeSecurity
+  },
+
+  verifySignature: (data, signature, publicKey, signatureEncode) => {
+    const verify = crypto.createVerify(signatureEncode);
+    verify.write(data);
+    verify.end();
+    return verify.verify(publicKey, signature, 'hex');
+  }
 };
