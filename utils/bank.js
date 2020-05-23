fcfdc594ca71be30af004d2c44486c04bf93a1c1
17021998 <ntquang17021998@gmail.com>
2020-05-24 00:51:16 +0700

    fix bank schemaschema

--- utils/bank.js
+++ utils/bank.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 let bank = new mongoose.Schema({
-  bank_name: String,
-  security_key: String, // out secret 
-  public_key: String, // public key for encrypt, decrypt
-  encode_type: String, // md5, sha256, sha512
-  encrypt_type: String, // sra, pgp
-  signature_format: String, // hex, base64, 
-  signature_encode_type: String, // md5, sha256, sha512,
+  bank_name: { type: String, required: true },
+  security_key: { type: String, required: true }, // out secret 
+  public_key: { type: String, required: true }, // public key for encrypt, decrypt
+  encode_type: { type: String, required: true }, // md5, sha256, sha512
+  encrypt_type: { type: String, required: true }, // sra, pgp
+  signature_format: { type: String, required: true }, // hex, base64, 
+  signature_encode_type: { type: String, required: true }, // md5, sha256, sha512,
   key_length: Number,
   passphrase: String, // option for pgp crypt
   private_key: String // test purpose
