6a61b8f3d21cc9d5cc081ea820940bda6966cc96
17021998 <ntquang17021998@gmail.com>
2020-05-24 21:57:35 +0700

    install axios

--- routes/test.routes.js
+++ routes/test.routes.js
@@ -1,9 +1,7 @@
 const { encrypt } = require('../utils/security');
-const mongoose = require('mongoose');
 const moment = require('moment');
 const crypto = require('crypto');
 const { find } = require('../models/linked_bank.model');
-const request = require('supertest');
 const axios = require('axios');
 const express = require('express');
 const router = express.Router();
@@ -13,7 +11,7 @@ router.get('/account', (req, res) => {
     let timestamp = moment().unix();
     let security_key = "test1";
     let _data = JSON.stringify(data, null, 2);
-    axios({
+    return axios({
         method: 'get',
         url: '/api/v1/linked/account',
         baseURL: 'https://s2q-ibanking.herokuapp.com',
@@ -28,27 +26,33 @@ router.get('/account', (req, res) => {
     }).catch(err => res.send(err));
 });
 
-router.post('/account', (req, res) => {
-    let data = { 
+router.post('/account', async (req, res) => {
+    let data = {
         account_number: req.body.data.account_number,
         amount: req.body.data.amount
-     };
+    };
     let timestamp = moment().unix();
     let security_key = "test1";
     let _data = JSON.stringify(data, null, 2);
-    axios({
-        method: 'post',
-        url: '/api/v1/linked/account',
-        baseURL: 'https://s2q-ibanking.herokuapp.com',
-        headers: { timestamp, security_key },
-        data: {
-            data,
-            hash: crypto.createHash('sha256').update(timestamp + _data + security_key).digest('hex')
-        }
-    }).then(function (response) {
-        console.log("RESPONSE: ", response);
-        res.send(response);
-    }).catch(err => res.send(err));
+    let bank = await find({ security_key });
+    let { private_key } = bank.attribute_data[0];
+    private_key = private_key.replace(/\\n/g, '\n');
+    try {
+        let result = await axios({
+            method: 'post',
+            url: '/api/v1/linked/account',
+            baseURL: 'https://s2q-ibanking.herokuapp.com',
+            headers: { timestamp, security_key },
+            data: {
+                data,
+                hash: crypto.createHash('sha256').update(timestamp + _data + security_key).digest('hex')
+            }
+        });
+        console.log(result);
+        res.send(result.data);
+    } catch (err) {
+        res.send(err);
+    }
 });
 
 module.exports = router;
